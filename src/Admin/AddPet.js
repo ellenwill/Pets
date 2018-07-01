40b613e1db3b497fbc3b968e62c143a89ecf7d23
letterbomb365 <l.long14@pitt.edu>
2018-07-01 15:12:23 -0400

    Links
    Firebase linked to addPet, made temp nav bar to check components

--- src/Admin/AddPet.js
+++ src/Admin/AddPet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import firebase from '../firebase';
 
 class AddPet extends Component{
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import AddPet from './Admin/AddPet'
 import PetList from './Visitor/PetList'
 import AdminLogin from './Admin/Login'
@@ -11,15 +11,21 @@ class App extends Component {
 
   render() {
     return(
+      <BrowserRouter>
       <div>
       THIS WILL BE EVERYWHERE
-      <BrowserRouter> //these are every PAGE that goes on the site. If you want something that is on every page, add as a div around
-        <Route path='/addPet' component={AddPet} /> //for the admin to add new pets
-        <Route path='/pets' component={PetList} /> //the pets that the visitor sees after selecting what kind of pet/ search
-        <Route path='/adminLogin' component={AdminLogin} /> //the page an admin goes to to login
-        <Route path='/vistorLogin' component={VisitorLogin} />
-      </BrowserRouter>
+      <Link to ='/addPet'> AddPet Link </Link>
+      <Link to ='/pets'> Pet Link </Link>
+      <Link to ='/adminLogin'> AdminLogin Link </Link>
+      <Link to ='/visitorLogin'> VisitorLogin Link </Link>
+        <div> {/*these are every PAGE that goes on the site. If you want something that is on every page, add as a div around*/}
+          <Route path='/addPet' component={AddPet} /> {/*for the admin to add new pets*/}
+          <Route path='/pets' component={PetList} /> {/*the pets that the visitor sees after selecting what kind of pet/ search*/}
+          <Route path='/adminLogin' component={AdminLogin} /> {/*the page an admin goes to to login*/}
+          <Route path='/vistorLogin' component={VisitorLogin} />
+        </div>
       </div>
+      </BrowserRouter>
     )
   }
 
