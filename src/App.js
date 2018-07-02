5d2a0bcfaf6f49c9c7afa75d35d14be71612639f
letterbomb365 <l.long14@pitt.edu>
2018-07-02 16:44:00 -0400

    Navigation Bar Component
    Made component for the site navigation bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import NavigationBar from './SiteParts/NavigationBar'
 import AddPet from './Admin/AddPet'
 import PetList from './Visitor/PetList'
 import AdminLogin from './Admin/Login'
@@ -13,11 +14,8 @@ class App extends Component {
     return(
       <BrowserRouter>
       <div>
-      THIS WILL BE EVERYWHERE
-      <Link to ='/addPet'> AddPet Link </Link>
-      <Link to ='/pets'> Pet Link </Link>
-      <Link to ='/adminLogin'> AdminLogin Link </Link>
-      <Link to ='/visitorLogin'> VisitorLogin Link </Link>
+      {/*THIS WILL BE EVERYWHERE*/}
+      <NavigationBar/>
         <div> {/*these are every PAGE that goes on the site. If you want something that is on every page, add as a div around*/}
           <Route path='/addPet' component={AddPet} /> {/*for the admin to add new pets*/}
           <Route path='/pets' component={PetList} /> {/*the pets that the visitor sees after selecting what kind of pet/ search*/}
---
+++ src/SiteParts/NavigationBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import AddPet from '../Admin/AddPet'
+import PetList from '../Visitor/PetList'
+import AdminLogin from '../Admin/Login'
+import VisitorLogin from '../Visitor/Login'
+
+const NavigationBar = () => {
+    return(
+        <div>
+        <AppBar title="Pet Adoption Site" position="static">
+            <Toolbar style={{backgroundColor: "#E04950"}}>
+                <Typography variant="title" >
+                ADOPET
+                <Link to ='/addPet'> AddPet Link </Link>
+                <Link to ='/pets'> Pet Link </Link>
+                <Link to ='/adminLogin'> AdminLogin Link </Link>
+                <Link to ='/visitorLogin'> VisitorLogin Link </Link>
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
+}
+export default NavigationBar;
