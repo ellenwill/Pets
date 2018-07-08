32d71a88256030eeb395294239242aaf617596c8
letterbomb365 <l.long14@pitt.edu>
2018-07-08 19:48:42 -0400

    began pet search bar

--- src/Visitor/PetList.js
+++ src/Visitor/PetList.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import firebase from '../firebase';
-import PetCards from '../SiteParts/PetCards'
+// import PetCards from '../SiteParts/PetCards'
+import PetSearchBar from '../SiteParts/PetSearchBar'
 
 class PetList extends Component{
 
     render(){
       return(
         <div>
-        <PetCards/>
+        <PetSearchBar/>
+
         </div>
       )
     }
