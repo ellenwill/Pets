7c15d409ba5829baa3d84399bae7596f8d93154f
midwayfair <jonspatton@gmail.com>
2018-07-12 12:24:43 -0400

    PetDisplay added to petProfile
    You have to double click on ViewMore to make it work, but at least it does work.

--- src/SiteParts/PetProfile.js
+++ src/SiteParts/PetProfile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import DBTools from '../DBTools/DBTools'
+import PetDisplay from '../Test/PetDisplay'
 
 const PetProfile = (props) => {
-return(<div>{props.match.params.petID}</div>)
+return(<div>{props.match.params.petID}<PetDisplay pet={new DBTools().getPetByID(props.match.params.petID)}/></div>)
 }
 
 export default PetProfile;
