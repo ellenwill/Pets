9ae9dce9796f9995965a2530aca6caf5408c1661
letterbomb365 <l.long14@pitt.edu>
2018-07-11 22:37:24 -0400

    Navbar w/ login dropdown

--- src/SiteParts/NavigationBar.js
+++ src/SiteParts/NavigationBar.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import AddPet from '../Admin/AddPet'
-import PetList from '../Visitor/PetList'
-import PetProviderList from '../Visitor/PetProviderList'
-import LoginForm from '../Visitor/LoginForm'
-import ALoginForm from '../Admin/ALoginForm'
-import Home from '../Visitor/Home'
-import Logo from './Logo.JPG'
 import TransparentLogo from "./Logo2.PNG";
 import * as routes from "../routes";
 import Button from '@material-ui/core/Button';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
+class NavigationBar extends React.Component {
+  state = {
+    anchorEl: null,
+  };
 
-//import Donate from '../Visitor/Donate'
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
-const NavigationBar = () => {
+  render() {
+    const { anchorEl } = this.state;
     return(
         <div style={{color:"#FFFFFF" }}>
         <AppBar title="Pet Adoption Site" position="static">
@@ -31,13 +35,28 @@ const NavigationBar = () => {
                   </div>
                   <div style={{flex:1}}></div>
                   <div>
-                      <AccountCircle style={{ color: 'white' }}/>
-                      <Link to = {routes.LOGIN}> Visitor Login </Link>
-                      <Link to = {routes.ADMIN_LOGIN}> Admin Login </Link>
+                      <Button
+                        aria-owns={anchorEl ? 'simple-menu' : null}
+                        aria-haspopup="true"
+                        onClick={this.handleClick}
+                        style={{fontSize:"18px", color:"white"}}
+                      >
+                        <AccountCircle style={{ color: 'white', fontSize:"18px" }}/>   Login
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={this.handleClose}
+                      >
+                        <MenuItem onClick={this.handleClose}><Link to = {routes.LOGIN}> Visitor Login </Link></MenuItem>
+                        <MenuItem onClick={this.handleClose}><Link to = {routes.ADMIN_LOGIN}> Admin Login </Link></MenuItem>
+                      </Menu>
                   </div>
             </Toolbar>
         </AppBar>
         </div>
     )
+  }
 }
 export default NavigationBar;
